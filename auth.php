5f42a850c6a16df35f8e60904c470f4fd3ca0fcf
Raphael Kathambana <maya12raph@gmail.com>
2023-06-09 10:19:25 +0300

    added env variables

--- auth.php
+++ auth.php
@@ -1,8 +1,11 @@
 <?php
-$user = "root";
-$password = "ChoosePassword";
-$dbName = "dogusers";
-$conn = new mysqli("localhost", $user, $password, $dbName);
+declare(strict_types=1);
+require_once 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
