4f053b5289880a90730cc83e254c896972ec4b79
Raphael Kathambana <maya12raph@gmail.com>
2023-06-14 12:44:27 +0300

    added logout logic

--- logout.php
+++ logout.php
@@ -1,5 +1,11 @@
-<?php 
+<?php
 
+// Perform any additional logout actions if needed
+
+// Unset or expire the session cookie
+$session_duration = -3600; // Set a negative duration to expire the cookie
+setcookie("session", "", time() + $session_duration);
+
+// Redirect to the login page or any other appropriate page
 header("Location: login.html");
 exit();
-
