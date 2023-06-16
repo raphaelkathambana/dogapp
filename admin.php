7c570615412188b40c82ffb841e59bd848c17607
raphaelkathambana <maya12raph@gmail.com>
2023-06-17 00:32:41 +0300

    created admin dashboard

---
+++ admin.php
@@ -0,0 +1,67 @@
+<?php
+require_once "auth.php";
+echo /*html*/"<br>" . "admin in the house";
+if (isset($_COOKIE["session"])) {
+    // User is logged in
+    $username = $_COOKIE["session"];
+} else {
+    // User is not logged in
+    header("Location: login.html");
+    exit();
+}
+
+$query = "SELECT * FROM users;";
+$result = mysqli_query($conn, $query);
+while ($row = mysqli_fetch_assoc($result)) {
+    echo /*html*/"<p>" . $row['id'] . /*html*/"</p>";
+    echo /*html*/"<p>" . $row['name'] . /*html*/"</p>";
+    echo /*html*/"<p>" . $row['email'] . /*html*/"</p>";
+}
+include_once "layout/header.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Document</title>
+</head>
+
+<body>
+<?php include_once "layout/navbar.php"; ?>
+
+    <table>
+    <caption>Table of Users</caption>
+        <tr>
+            <th>#ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+
+        </tr>
+        <?php
+            $queries = "SELECT * FROM users;";
+            $users_res = $conn->query($queries);
+            if ($users_res->num_rows > 0) {
+                while ($rows = mysqli_fetch_assoc($users_res)): ?>
+                    <tr>
+                    <td>
+                        <?php print $rows['id']; ?>
+                    </td>
+                    <td>
+                        <?php print $rows["name"]; ?>
+                    </td>
+                    <td>
+                        <?php print $rows["email"]; ?>
+                    </td>
+                    <td>[ <a href="">Edit</a> ] [ <a href="">Del</a> ]</td>
+                    </tr>
+                <?php endwhile;
+            } else {
+                echo /*html*/"<br>" . "Nope";
+            } ?>
+    </table>
+<?php include_once "layout/footer.php"; ?>
+
+</body>
+
+</html>
