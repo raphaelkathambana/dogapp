4633965bf2d6e8756313b7950a4b8957192c0490
Raphael Kathambana <maya12raph@gmail.com>
2023-06-16 17:06:32 +0300

    prolonged cookie session time logic

--- authenticate.php
+++ authenticate.php
@@ -5,6 +5,7 @@ $password = "";
 if (isset($_POST["login"])) {
     $email = $_POST["email"];
     $password = $_POST["password"];
+    $remember_me = isset($_POST["remember-me"]) ? true : false;
     var_dump($_POST);
 } else {
     echo /*html*/"<br>" . "Nope";
@@ -15,7 +16,7 @@ $query = "SELECT * FROM users WHERE email = '$email' AND password = '$password';
 $result = mysqli_query($conn, $query);
 if (mysqli_num_rows($result) > 0) {
     $row = mysqli_fetch_assoc($result);
-    $session_duration = 180; // 1 hour (in seconds)
+    $session_duration = $remember_me ? 86400 * 30 : 180;
     setcookie("session", $row["name"], time() + $session_duration);
     header("Location: indexing.php");
 } else {
