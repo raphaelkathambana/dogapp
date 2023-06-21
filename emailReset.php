6dd0bfde8a89e0c13d63e18d30cc9681a07430db
Raphael Kathambana <maya12raph@gmail.com>
2023-06-21 21:03:15 +0300

    merged email and emailReset

--- emailReset.php
+++ emailReset.php
@@ -1,4 +1,14 @@
 <?php
+require_once 'vendor/autoload.php';
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+//Load Composer's autoloader
 
 if (isset($_POST['verify'])) {
     if (!filter_var($_POST['user-email'], FILTER_VALIDATE_EMAIL)) {
@@ -6,19 +16,37 @@ if (isset($_POST['verify'])) {
     } else {
         var_dump($_POST);
     }
-} else  {
+} else {
     echo /*html*/"<br>" . "Nope";
 }
+require_once "email.php";
 
-// require_once "email.php";
-// try {
-//     $mail->addAddress($_POST['user-email']); //Name is optional
-//     $mail->Subject = 'Reset Password';
-//     $mail->Body = 'Enter the following key to reset your password\n UUIIDDK';
-//     $mail->AltBody = 'Alt Reset Password Using Email';
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com'; //gmail SMTP server
+    $mail->SMTPAuth = true;
 
-//     $mail->send();
-//     echo 'Message has been sent'; header('Location: verifyKey.php')
-// } catch (Exception $e) {
-//     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-// }
+    $mail->Port = 465; //SMTP port
+    $mail->SMTPSecure = "ssl";
+    //Enable SMTP authentication
+    $mail->Username = $_ENV['SMTP_USERNAME']; // SMTP username
+    $mail->Password = $_ENV['SMTP_PASSWORD']; //SMTP password
+    $mail->SMTPSecure = "ssl";
+
+    //Recipients
+    $mail->setFrom('maya12raph@gmail.com', 'Pawsitive Puppers');
+    $mail->addAddress($_POST['user-email']); //Name is optional
+    $mail->Subject = 'Reset Password';
+    $mail->Body = 'Enter the following key to reset your password\n UUIIDDK';
+    $mail->AltBody = 'Alt Reset Password Using Email';
+    //Content
+    $mail->isHTML(false); //Set email format to HTML
+
+    $mail->send();
+    echo 'Message has been sent';
+    header('Location: validateKey.php');
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
