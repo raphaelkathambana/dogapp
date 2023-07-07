43ac767ae93070dcba10fa3edfa641db2b826abf
raphaelkathambana <maya12raph@gmail.com>
2023-07-07 23:56:01 +0300

    updated user table

--- db.sql
+++ db.sql
@@ -8,12 +8,14 @@ create table if not exists users(
     password varchar(100) not null default "",
     created_at datetime not null default current_timestamp(),
     updated_at datetime not null default current_timestamp() ON UPDATE current_timestamp(),
+    admin boolean, -- indicated whether the user is also an admin or not. -- "0" being "is not an admin"
     primary key (id),
     UNIQUE KEY email (email)
 );
-
-insert into users (name, email, password) values ("panther", "panther@hot.com", "QmyVF&zz${r{1L`=");
+insert into users (name, email, password, admin) values ("panther", "panther@hot.com", "QmyVF&zz${r{1L`=", 1);
+insert into users (name, email, password, admin) values ("panther", "panther@hot.com", "ECUYR`)HR^sNmH8u", 1);
 select * from users;
+
 drop table if exists dog_info;
 create table if not exists dog_info(
     id INT(3) not null,
@@ -24,6 +26,7 @@ create table if not exists dog_info(
      foreign key (id) references users(id)
 );
 INSERT INTO dog_info VALUES (1, "Sparky", "German Shepherd", 9, "Male");
+SELECT * FROM dog_info;
 
 DROP TABLE if exists noteContent;
 CREATE TABLE IF NOT EXISTS noteContent (
@@ -32,9 +35,5 @@ CREATE TABLE IF NOT EXISTS noteContent (
     note_description varchar(1024),
     date_of_note datetime
 );
-
 INSERT INTO noteContent (title,note_description,date_of_note) VALUES ("Note 2", "This is an Example of a note", "2023-06-17");
-
-delete from noteContent where id = 4;
-
 SELECT * FROM notecontent;
