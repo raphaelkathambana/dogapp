07c25cd659c48c3e72b2b7b78596cc16f84c2d3f
Raphael Kathambana <maya12raph@gmail.com>
2023-09-12 13:44:08 +0300

    initial file for setting email variables

---
+++ emailDetails.php
@@ -0,0 +1,26 @@
+<?php 
+
+$emails = [
+    "emailOne" => [
+        "subject" => "Contact Form Submission",
+        "messagePlain" => "Name: $name\nEmail: $email\nMessage: $message",
+        "messageHtml" => "<p>Name: $name</p><p>Email: $email</p><p>Message: $message</p>",
+        "receiverName" => "Pawsitive Puppers",
+        "receiverEmail" => "<EMAIL>"
+    ],
+    "emailTwo" => [
+        "subject" => "Contact Form Submission",
+        "messagePlain" => "Name: $name\nEmail: $email\nMessage: $message",
+        "messageHtml" => "<p>Name: $name</p><p>Email: $email</p><p>Message: $message</p>",
+        "receiverName" => "Pawsitive Puppers",
+        "receiverEmail" => "<EMAIL>"
+    ],
+    "emailThree" => [
+        "subject" => "Contact Form Submission",
+        "messagePlain" => "Name: $name\nEmail: $email\nMessage: $message",
+        "messageHtml" => "<p>Name: $name</p><p>Email: $email</p><p>Message: $message</p>",
+        "receiverName" => "Pawsitive Puppers",
+        "receiverEmail" => "<EMAIL>"
+    ]
+];
+
