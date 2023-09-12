a8829871c29bc8637a5aa28be2d649376a1ed068
Raphael Kathambana <maya12raph@gmail.com>
2023-09-12 13:49:32 +0300

    added and edited send email fn

--- makeContact.php
+++ makeContact.php
@@ -1,9 +1,23 @@
 <?php
+require_once 'vendor/autoload.php';
 
 if (isset($_POST['contact'])) {
     echo /*html*/ "<pre>";
     var_dump($_POST);
     echo /*html*/ "</pre>";
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $subject = "Contact Form Submission";
+    $messagePlain = "Name: $name\nEmail: $email\nMessage: $message";
+    $messageHtml = "<p>Name: $name</p><p>Email: $email</p><p>Message: $message</p>";
+
+    $mail = new \SendMail();
+    //    mail($to, $subject, $messagePlain) or die("Error!"); //send plain text email to receiver.
+    $mail->sendAnEmail($subject, $messagePlain, $messageHtml, $name, $email);
+    // header("Location: contact.php?success=true");
 } else {
     echo "Nope" . /*html*/ "<br>";
 }
--- sendgridMail.php
+++ sendgridMail.php
@@ -5,22 +5,27 @@ require_once 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-function sendAnEmail($subject, $messagePlain, $messageHtml, $receiverName, $receiverEmail) {
-    $email = new \SendGrid\Mail\Mail();
-    $mailEmail = $_ENV['EMAIL_USERNAME']; // SMTP username
-    
-    $email->setFrom($mailEmail, "Pawsitive Puppers");
-    $email->setSubject($subject);
-    $email->addTo($receiverEmail, $receiverName);
-    $email->addContent("text/plain", $messagePlain);
-    $email->addContent("text/html", $messageHtml);
-    $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
-    try {
-        $response = $sendgrid->send($email);
-        print $response->statusCode() . "\n";
-        print_r($response->headers());
-        print $response->body() . "\n";
-    } catch (Exception $e) {
-        echo 'Caught exception: '. $e->getMessage() ."\n";
+class SendMail {
+
+    public function sendAnEmail($subject, $messagePlain, $messageHtml, $receiverName, $receiverEmail) {
+        $email = new \SendGrid\Mail\Mail();
+        $mailEmail = $_ENV['EMAIL_USERNAME']; // SMTP username
+
+        $email->setFrom($mailEmail, "Pawsitive Puppers");
+        $email->setSubject($subject);
+        $email->addTo($receiverEmail, $receiverName);
+        $email->addContent("text/plain", $messagePlain);
+        $email->addContent("text/html", $messageHtml);
+        $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
+        echo "Ok, here goes" . /*html*/ "<br>";
+        try {
+            $response = $sendgrid->send($email);
+            print $response->statusCode() . "\n";
+            print_r($response->headers());
+            print $response->body() . "\n";
+        } catch (Exception $e) {
+            echo "nope" . /*html*/ "<br>";
+            echo 'Caught exception: ' . $e->getMessage() . "\n";
+        }
     }
 }
